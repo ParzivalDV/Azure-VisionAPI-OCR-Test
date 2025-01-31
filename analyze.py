83d70e1f9ff95c25c2cc61119ea9b5515696fd03
Prajwal D. Vishwanath <43853402+ParzivalDV@users.noreply.github.com>
2025-01-31 16:03:55 -0600

    Add files via upload

---
+++ analyze.py
@@ -0,0 +1,59 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+import time
+
+# Define a dictionary to store the credentials
+credentials = {}
+
+# Read the key-value pairs from the file
+with open("api-key.txt", "r") as file:
+    for line in file:
+        key, value = line.strip().split("=", 1)  # Split the line into key and value
+        credentials[key] = value
+
+# Access the credentials
+api_key = credentials.get("key")
+api_endpoint = credentials.get("endpoint")
+
+credentials = CognitiveServicesCredentials(api_key)
+
+client = ComputerVisionClient(
+    endpoint=api_endpoint,
+    credentials=credentials
+)
+
+def read_image(uri):
+    numberOfCharsInOperationId = 36
+    maxRetries = 10
+
+    # SDK call
+    rawHttpResponse = client.read(uri, language="en", raw=True)
+
+    # Get ID from returned headers
+    operationLocation = rawHttpResponse.headers["Operation-Location"]
+    idLocation = len(operationLocation) - numberOfCharsInOperationId
+    operationId = operationLocation[idLocation:]
+
+    # SDK call
+    result = client.get_read_result(operationId)
+    
+    # Try API
+    retry = 0
+    
+    while retry < maxRetries:
+        if result.status.lower () not in ['notstarted', 'running']:
+            break
+        time.sleep(1)
+        result = client.get_read_result(operationId)
+        
+        retry += 1
+    
+    if retry == maxRetries:
+        return "max retries reached"
+
+    if result.status == OperationStatusCodes.succeeded:
+        res_text = " ".join([line.text for line in result.analyze_result.read_results[0].lines])
+        return res_text
+    else:
+        return "error"
